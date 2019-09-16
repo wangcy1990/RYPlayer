255e0dd0f01ad4a13a1b6d1f7108168056375486
wangcy <wangcy1990x@163.com>
2019-09-16 09:57:54 +0800

    修改截图接口

---
+++ RYPlayer/RYPlayerSDK.framework/Headers/RYFloatView.h
@@ -0,0 +1,13 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface RYFloatView : UIView
+
+/// The parent View
+@property(nonatomic, weak) UIView *parentView;
+
+/// Safe margins, mainly for those with Navbar and tabbar
+@property(nonatomic, assign) UIEdgeInsets safeInsets;
+
+@end
